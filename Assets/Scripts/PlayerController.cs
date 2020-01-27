a2e2318e111e84ef28f1103bff1b79442f5de9a9
Jenny Lei <jenny.h.lei@hotmail.com>
2020-01-27 14:37:59 -0500

    movement actions and temporary platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpHeight;
+    public bool isGrounded = true;
+
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>() ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.05F, 0, 0);
+        jump();
+        move();
+    }
+
+    void move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb2d.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+
+            // isGrounded = false;
+        }
     }
 }
