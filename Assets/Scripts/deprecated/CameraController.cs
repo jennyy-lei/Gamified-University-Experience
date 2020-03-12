1f4d7707a4b77d1bbdd8167f538b682730b3c9c5
yiti_ <yitijiang@gmail.com>
2020-03-11 21:58:41 -0400

    moved old script to deprecated folder

---
+++ Assets/Scripts/deprecated/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float lerp_speed = 5f;
+
+    public float topLimit;
+    public float bottomLimit;
+    public float rightLimit;
+    public float leftLimit;
+
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position  + offset, lerp_speed) + new Vector3(0, 0, -5);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x,leftLimit,rightLimit),
+            Mathf.Clamp(transform.position.y,bottomLimit,topLimit),
+            transform.position.z
+        );
+        // transform.position = player.transform.position + offset;
+    }
+}
