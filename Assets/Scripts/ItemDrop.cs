847e767ccdf4f0d5dbd7fc012b8060116f6a6c0b
Jenny Lei <jenny.h.lei@hotmail.com>
2020-03-17 21:58:26 -0400

    goose drops predertermined items on death

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] itemList;
+
+    private int DropRange = 2;
+    
+    void OnDestroy()
+    {
+        Drop();
+    }
+
+    private void Drop()
+    {
+        int randNum = Random.Range(0, 100);
+
+        GenItem(itemList[0]);
+        GenItem(itemList[0]);
+        GenItem(itemList[0]);
+    }
+
+    private void GenItem(GameObject item)
+    {
+        GameObject obj = (GameObject)Instantiate(item, gameObject.transform.position, gameObject.transform.rotation);
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-DropRange, DropRange), Random.Range(0, DropRange));
+    }
+}
