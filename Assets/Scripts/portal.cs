62a2fd0ca219d78103ea22c7e88cdd2a066e8cb3
Jenny Lei <jenny.h.lei@hotmail.com>
2020-03-18 16:06:57 -0400

    portal prefab

---
+++ Assets/Scripts/portal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private GameObject textCanvas;
+
+    private Vector2 textStartPosition;
+    
+    void Start()
+    {
+        textCanvas.SetActive(false);
+
+        textStartPosition = textCanvas.transform.position;
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(gameObject.transform.position, player.position);
+        if(distance < 3) {
+            textCanvas.SetActive(true);
+        } else {
+            textCanvas.SetActive(false);
+        }
+
+        textCanvas.transform.position = new Vector2(textStartPosition.x, textStartPosition.y + Mathf.Sin(Time.time * 3) * 0.05f);
+    }
+}
