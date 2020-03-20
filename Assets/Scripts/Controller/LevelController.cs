18b0eb7822d47df9e196711bb60007cdcb6a1e7e
yiti_ <yitijiang@gmail.com>
2020-03-20 00:58:38 -0400

    Simple switch scene script

---
+++ Assets/Scripts/Controller/LevelController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private int curSceneIndex;
+    public bool test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curSceneIndex = 0;
+    }
+
+    void Update(){
+        if(test){
+            test = false;
+            switchScene(1);
+        }
+    } 
+
+    public void switchScene(int index){
+            SceneManager.LoadScene(index);
+            curSceneIndex = index;
+    }
+
+}
