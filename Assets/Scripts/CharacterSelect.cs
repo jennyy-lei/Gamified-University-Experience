4dfc4cfa1bdb89e620d46b2b20a8e3469c43264c
Jenny Lei <jenny.h.lei@hotmail.com>
2020-03-21 18:57:37 -0400

    replace player gameobject with correct playerSprite

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject player;
+
+    public void Select(GameObject button)
     {
-        
+        Debug.Log(button);
+        GameObject newObj = (GameObject)Instantiate(button.transform.GetChild(1).gameObject, player.transform.GetChild(0).position, player.transform.GetChild(0).rotation);
+        newObj.transform.localScale = new Vector3(1, 1, 1);
+
+        Destroy(player.transform.GetChild(0).gameObject);
+
+        newObj.transform.parent = player.transform;
+        newObj.transform.SetSiblingIndex(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        Debug.Log("start!");
     }
 }
