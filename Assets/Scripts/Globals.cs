180fde425692779dcd58183cb2459f140b1cc776
Jenny Lei <jenny.h.lei@hotmail.com>
2020-03-27 20:04:46 -0400

    trader prefab

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public static class Globals
 {
     private static readonly GameObject[] charList = Resources.LoadAll<GameObject>("Units/PlayerSprites/");
-    private static int charIndex = 1;
+    private static int charIndex = 0;
 
+    // getters
     public static GameObject[] getCharList () => charList;
+    public static int getCharIndex () => charIndex;
+    
+    // setters
     public static void setCharIndex (int index) {
         if (index >= 0 && index <= charList.Length)
             charIndex = index;
     }
-    public static int getCharIndex () => charIndex;
 }
---
+++ Assets/Scripts/Trader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trader : MonoBehaviour
+{
+    private Transform player;
+
+    [SerializeField]
+    private GameObject textCanvas;
+    
+    [SerializeField]
+    private GameObject nameText;
+    [SerializeField]
+    private GameObject convoText;
+
+    private Vector2 textStartPosition;
+    
+    void Start()
+    {
+        textCanvas.SetActive(false);
+        textStartPosition = textCanvas.transform.position;
+
+        nameText.SetActive(true);
+        convoText.SetActive(false);
+    }
+
+    void Awake()
+    {
+        Debug.Log("hello?");
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(gameObject.transform.position, player.position);
+        if(distance < 3) {
+            textCanvas.SetActive(true);
+            if (distance < 1) {
+                nameText.SetActive(false);
+                convoText.SetActive(true);
+
+                Input();
+            } else {
+                nameText.SetActive(true);
+                convoText.SetActive(false);
+            }
+        } else {
+            textCanvas.SetActive(false);
+        }
+        
+        textCanvas.transform.position = new Vector2(textStartPosition.x, textStartPosition.y + Mathf.Sin(Time.time * 3) * 0.05f);
+    }
+
+    private void Input()
+    {
+        
+    }
+}
