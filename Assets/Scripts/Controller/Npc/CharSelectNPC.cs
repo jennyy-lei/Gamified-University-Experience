2c4f314a138af05147627973234a5d3e942660f4
yiti_ <yitijiang@gmail.com>
2020-04-30 17:41:25 -0400

    reorganized script files

---
+++ Assets/Scripts/Controller/Npc/CharSelectNPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelectNPC : Npc
+{
+    [SerializeField]
+    private GameObject selectScreen;
+
+    override public void Open() {
+        selectScreen.SetActive(true);
+    }
+
+    override public void Close() {
+        selectScreen.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Scene/Globals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    private static readonly GameObject[] charList = Resources.LoadAll<GameObject>("Units/PlayerSprites/");
+    private static int charIndex = 0;
+
+    private static readonly GameObject[] itemList = Resources.LoadAll<GameObject>("Items/");
+
+    // getters
+    public static GameObject[] getCharList () => charList;
+    public static int getCharIndex () => charIndex;
+    public static GameObject[] getItemList () => itemList;
+    
+    // setters
+    public static void setCharIndex (int index) {
+        if (index >= 0 && index <= charList.Length)
+            charIndex = index;
+    }
+    // other functions
+    public static float totalDropChance() {
+        float max = 0;
+        foreach (GameObject item in itemList) {
+            //if(max <= item.GetComponent<IItem>().dropChance)
+                max += item.GetComponent<IItem>().dropChance;
+        }
+
+        return max;
+    }
+}
+
+public static class StrConstant
+{
+    public const string playerSpawnAddr = "GameManager/PlayerSpawnPoint";
+    public const string platformTag = "Platform";
+    public const string playerTag = "Player";
+    public const string enemyTag = "Enemy";
+    public const string bulletTag = "Bullet";
+}
