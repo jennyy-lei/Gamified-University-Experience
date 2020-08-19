4ba773b3a58cf3b8edb36c3e76bff2afc3f62816
yiti_ <yitijiang@gmail.com>
2020-08-19 17:16:48 -0400

    added traderNPC

--- Assets/Scripts/Controller/Npc/TraderNPC.cs
+++ Assets/Scripts/Controller/Npc/TraderNPC.cs
@@ -4,4 +4,14 @@ using UnityEngine;
 
 public class TraderNPC : Npc
 {
+    [SerializeField]
+    private GameObject selectScreen;
+
+        override public void Open() {
+        selectScreen.SetActive(true);
+    }
+
+    override public void Close() {
+        selectScreen.SetActive(false);
+    }
 }
